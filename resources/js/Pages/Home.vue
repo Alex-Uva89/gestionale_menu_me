2e5431fff5e4325a378deb41f4882dd6731b09dd
Alex-Uva89 <a.uva1988@gmail.com>
2024-05-29 09:25:09 +0200

    [ADD]ADD_template_welcome

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -47,6 +47,7 @@ function updateCurrentPageComponent(value) {
 
 <template>
     <Head title="Mamma Elvira" />
+    <div class="relative">
       <div class="bg-gray-50 flex dark:bg-gray-800 h-screen">
         <Sidebar :messages="messages" @venue-name="updateSelectedVenueName" @venue-color="updateSelectedVenueColor" @value-button="updateSelectedValueButton" />
 
@@ -54,7 +55,10 @@ function updateCurrentPageComponent(value) {
           <Header class="col-9" v-if="selectedVenueName? selectedVenueName : selectedValueButton" :selectedVenueName="selectedVenueName" :selectedVenueColor="selectedVenueColor" :selectedValueButton="selectedValueButton" :newMessage="newMessage"/>
           <Inbox :messages="messages" v-if="selectedValueButton === 'inbox'" @message-not-read="updateNewMessage"/>
           
-          <Welcome v-if="currentPageComponent === ''"/>
+          <Welcome 
+            v-if="currentPageComponent === ''"
+            @open-modal-instruction="toggleModalInstruction"
+          />
           <component 
             v-else
             :is="currentPageComponent" 
@@ -71,6 +75,22 @@ function updateCurrentPageComponent(value) {
             :key="componentKey"
             />     
         </div>
+
+      </div>
+      <div @click="toggleModalInstruction()" class="z-50 modal-instruction absolute w-screen h-screen top-0 right-0 bg-opacity-50 bg-black" :class="{ visible: openModalInstruction }">
+        <div class="focus-modal" :class="{ visible: openModalInstruction }">
+        </div>
+        <div class="focus-instruction" :class="{ visible: openModalInstruction }">
+         <span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-arrow-return-left text-2xl" viewBox="0 0 26 26">
+            <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5"/>
+          </svg>
+         </span>
+         <span class="text-6xl">
+           clicca qui per iniziare
+         </span>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -85,6 +105,56 @@ function updateCurrentPageComponent(value) {
         scrollbar-width: thin;
     }
 }
+.modal-instruction {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.5);
+    visibility: hidden;
+    opacity: 0;
+    transition: visibility .5s, opacity .5s linear;
+}
+
+.modal-instruction.visible {
+    visibility: visible;
+    opacity: 1;
+}
+
+.focus-modal {
+    position: absolute;
+    top: 18%;
+    left: 15px;
+    width: 235px;
+    height: 350px;
+    background: transparent;
+    border-radius: 10px;
+    border: 4px solid #fff;
+    box-shadow: 0 0 0 9999px rgba(0, 0, 0, 0.8);
+    visibility: hidden;
+    opacity: 0;
+    transition: visibility .5s, opacity .5s linear;
+}
+
+.focus-modal.visible {
+    visibility: visible;
+    opacity: 1;
+}
+
+.focus-instruction {
+    position: absolute;
+    top: 30%;
+    left: 300px;
+    color: #fff;
+    transition: visibility 2s, opacity 2s linear;
+}
+
+.focus-instruction.visible {
+    visibility: visible;
+    opacity: 1;
+}
+
 </style>
 
 <script>
@@ -100,7 +170,13 @@ export default {
   data() {
     return {
       componentKey: 0,
+      openModalInstruction: false,
     };
+  },
+  methods: {
+    toggleModalInstruction() {
+      this.openModalInstruction = !this.openModalInstruction;
+    },
   },
 }
 
