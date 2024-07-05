055f88a1a0f8108f80e3bde15efab950d98c6b28
Alex-Uva89 <a.uva1988@gmail.com>
2024-07-05 15:52:44 +0200

    new vite.config BASE

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
@@ -18,15 +17,18 @@ export default defineConfig({
             },
         }),
     ],
+    server: {
+        https: true,
+    },
     build: {
-        outDir: 'public/build',
         rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'resources/js/app.js'),
+            output: {
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash][extname]',
+                manualChunks: undefined,
             },
         },
     },
-    server: {
-        https: true,
-    }
+    base: 'https://majestic-isle-royale-01245-f4e167ad8dbf.herokuapp.com/',
 });
