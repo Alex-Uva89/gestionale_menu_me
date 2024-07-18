48054f884dfec42c2e6d44fcccb456f300a2ca8e
Alex-Uva89 <a.uva1988@gmail.com>
2024-07-18 15:07:04 +0200

    [fix] web.php fix route for imgs

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,37 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MessageController;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\File;
 
-// Route principale che utilizza Inertia per rendere il componente Home
+// MAIN route controller
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+// Route image storage
+Route::get('/storage/{filename}', function ($filename) {
+    $path = storage_path('app/public/immagini/' . $filename);
 
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
+
+
+// Route with Inertia.js
 Route::get('/{any}', function () {
     return Inertia::render('Home');
 })->where('any', '.*');
 
-
 Route::put('/messages/{id}', [MessageController::class, 'update']);
 Route::delete('/messages/{id}', [MessageController::class, 'destroy']);
 
-
-
-
 require __DIR__.'/auth.php';
+
