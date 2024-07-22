60a6860a392ed5c686d603cf46e4f48309e95dda
Alex-Uva89 <a.uva1988@gmail.com>
2024-07-22 12:01:57 +0200

    REMOVE images from filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,13 +45,6 @@ return [
             'throw' => false,
         ],
 
-        'images' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public/immagini'),
-            'url' => env('APP_URL').'/storage/immagini',
-            'visibility' => 'public',
-        ],
-
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
