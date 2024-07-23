348325fe667f4d361b7f12f32b2534b88a071047
Alex-Uva89 <a.uva1988@gmail.com>
2024-07-23 10:52:19 +0200

    fix devtool

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,17 +13,19 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
+        const app = createApp({ render: () => h(App, props) });
+
+        app.use(plugin)
             .use(ZiggyVue)
             .mount(el);
+
+        if (process.env.NODE_ENV === 'production') {
+            app.config.devtools = true;
+        }
+
+        return app;
     },
     progress: {
         color: '#4B5563',
     },
 });
-
-if (process.env.NODE_ENV === 'production') {
-    app.config.devtools = true;
-}
-//:TODO
