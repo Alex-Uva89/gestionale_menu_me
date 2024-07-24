d2b3d91f24be06d9d026bd03ae8531751821a4ff
Alex-Uva89 <a.uva1988@gmail.com>
2024-07-24 12:07:38 +0200

    errore Allergens insert control URL

--- resources/js/PartialsHome/Allergens.vue
+++ resources/js/PartialsHome/Allergens.vue
@@ -239,31 +239,36 @@ export default {
             this.form.icon = e.target.files[0];
         },
         uploadImageIcon(img) {
-                let body = new FormData()
-                body.set('key', 'b77fe7e58631e53150bce61c6ad37bb5')
-                body.append('image', img)
+            let body = new FormData();
+            body.set('key', 'b77fe7e58631e53150bce61c6ad37bb5');
+            body.append('image', img);
 
-                return axios({
+            return axios({
                 method: 'post',
                 url: 'https://api.imgbb.com/1/upload',
                 data: body
-                })
+            });
         },
         createAllergen(name) {
             const formData = new FormData();
             formData.append('name', name);
 
-            console.log('ICON CONTENT')
-            console.log(this.form.icon)
+            console.log('ICON CONTENT');
+            console.log(this.form.icon);
             this.uploadImageIcon(this.form.icon)
                 .then(response => {
                     const url = response.data.data.url;
-                    formData.append('icon', url);
-                    return axios.post('/api/allergens', formData, {
-                        headers: {
-                            'Content-Type': 'multipart/form-data'
-                        }
-                    });
+                    console.log('Uploaded Image URL:', url);  // Debug the image URL
+                    if (url) {
+                        formData.append('icon', url);
+                        return axios.post('/api/allergens', formData, {
+                            headers: {
+                                'Content-Type': 'multipart/form-data'
+                            }
+                        });
+                    } else {
+                        throw new Error('Image upload failed: URL is invalid');
+                    }
                 })
                 .then(response => {
                     // Gestione della risposta della creazione dell'allergene
