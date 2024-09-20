9434729f88ad2ba99f1f8adf1c3e860874fed7f1
Alex-Uva89 <a.uva1988@gmail.com>
2024-09-20 15:15:38 +0200

    Refactor Loading.vue component to add loading overlay and spinner

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
 import { ref } from 'vue';
+import Loading from '@/Components/Loading.vue';
 import Sidebar from '@/Partials/Sidebar.vue';
 import Header from '@/Partials/Header.vue';
 import Inbox from '@/PartialsHome/Inbox.vue';
@@ -50,68 +51,72 @@ function updateCurrentPageComponent(value) {
 <template>
     <Head title="Mamma Elvira" />
     <div class="relative">
-      <div class="bg-gray-50 flex dark:bg-gray-800 h-screen">
-        <Sidebar :messages="messages" @venue-name="updateSelectedVenueName" @venue-color="updateSelectedVenueColor" @value-button="updateSelectedValueButton" />
+      <Loading v-if="isLoading" /> <!-- Mostra il caricamento -->
+      <div v-else>
 
-        <div class="main-app flex">
-          <Header class="col-9" v-if="selectedVenueName? selectedVenueName : selectedValueButton" :selectedVenueName="selectedVenueName" :selectedVenueColor="selectedVenueColor" :selectedValueButton="selectedValueButton" :newMessage="newMessage"/>
-          <Inbox :messages="messages" v-if="selectedValueButton === 'inbox'" @message-not-read="updateNewMessage"/>
-          <Welcome 
-            v-if="currentPageComponent === ''"
-            @open-modal-instruction="toggleModalInstruction"
-            />
-          <component 
-            v-else
-            :allergens="allergens"
-            :allergensDishes="allergensDishes"
-            :allergensDrinks="allergensDrinks"
-            :is="currentPageComponent" 
-            :selectedVenueName="selectedVenueName" 
-            :selectedVenueColor="selectedVenueColor" 
-            :category_laCucina="category_laCucina" 
-            :category_scante="category_scante"
-            :category_enoteca="category_enoteca"
-            @update:category_laCucina="category_laCucina=$event"
-            @update:category_scante="category_scante=$event"
-            @update:category_enoteca="category_enoteca=$event"
-            :categories="categories" 
-            :dish_enoteca_category="dish_enoteca_category"
-            :drink_enoteca_category="drink_enoteca_category"
-            :dish_laCucina_category="dish_laCucina_category"
-            :drink_laCucina_category="drink_laCucina_category"
-            :dish_scante_category="dish_scante_category"
-            :drink_scante_category="drink_scante_category"
-            :messages="messages"
-            @dishAdded="componentKey += 1"
-            @drinkAdded="componentKey += 1"
-            :key="componentKey"
-            :receips="receips"
-            :updateIsShowStatus="updateIsShowStatus"
-            @changePage="updateCurrentPageComponent"
-            @changeHeader="updateSelectedValueButton"
-            :drinks="drinks"
-            :dishes="dishes"
-            :pairingsEnoteca="pairingsEnoteca"
-            :pairingsScante="pairingsScante"
-            :pairingsLaCucina="pairingsLaCucina"
-            />     
-        </div>
-  
+        <div class="bg-gray-50 flex dark:bg-gray-800 h-screen">
+          <Sidebar :messages="messages" @venue-name="updateSelectedVenueName" @venue-color="updateSelectedVenueColor" @value-button="updateSelectedValueButton" />
 
-        
-      </div>
-      <div @click="toggleModalInstruction()" class="z-50 modal-instruction absolute w-screen h-screen top-0 right-0 bg-opacity-50 bg-black" :class="{ visible: openModalInstruction }">
-        <div class="focus-modal cursor-pointer" :class="{ visible: openModalInstruction }">
+          <div class="main-app flex">
+            <Header class="col-9" v-if="selectedVenueName? selectedVenueName : selectedValueButton" :selectedVenueName="selectedVenueName" :selectedVenueColor="selectedVenueColor" :selectedValueButton="selectedValueButton" :newMessage="newMessage"/>
+            <Inbox :messages="messages" v-if="selectedValueButton === 'inbox'" @message-not-read="updateNewMessage"/>
+            <Welcome 
+              v-if="currentPageComponent === ''"
+              @open-modal-instruction="toggleModalInstruction"
+              />
+            <component 
+              v-else
+              :allergens="allergens"
+              :allergensDishes="allergensDishes"
+              :allergensDrinks="allergensDrinks"
+              :is="currentPageComponent" 
+              :selectedVenueName="selectedVenueName" 
+              :selectedVenueColor="selectedVenueColor" 
+              :category_laCucina="category_laCucina" 
+              :category_scante="category_scante"
+              :category_enoteca="category_enoteca"
+              @update:category_laCucina="category_laCucina=$event"
+              @update:category_scante="category_scante=$event"
+              @update:category_enoteca="category_enoteca=$event"
+              :categories="categories" 
+              :dish_enoteca_category="dish_enoteca_category"
+              :drink_enoteca_category="drink_enoteca_category"
+              :dish_laCucina_category="dish_laCucina_category"
+              :drink_laCucina_category="drink_laCucina_category"
+              :dish_scante_category="dish_scante_category"
+              :drink_scante_category="drink_scante_category"
+              :messages="messages"
+              @dishAdded="componentKey += 1"
+              @drinkAdded="componentKey += 1"
+              :key="componentKey"
+              :receips="receips"
+              :updateIsShowStatus="updateIsShowStatus"
+              @changePage="updateCurrentPageComponent"
+              @changeHeader="updateSelectedValueButton"
+              :drinks="drinks"
+              :dishes="dishes"
+              :pairingsEnoteca="pairingsEnoteca"
+              :pairingsScante="pairingsScante"
+              :pairingsLaCucina="pairingsLaCucina"
+              />     
+          </div>
+    
+
+          
         </div>
-        <div class="focus-instruction flex flex-col gap-5" :class="{ visible: openModalInstruction }">
-         <span class="text-6xl cursor-pointer">
-           clicca qui per iniziare
-         </span>
-         <span>
-          <svg xmlns="https://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-arrow-return-left text-2xl cursor-pointer" viewBox="0 0 26 26">
-            <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5"/>
-          </svg>
-         </span>
+        <div @click="toggleModalInstruction()" class="z-50 modal-instruction absolute w-screen h-screen top-0 right-0 bg-opacity-50 bg-black" :class="{ visible: openModalInstruction }">
+          <div class="focus-modal cursor-pointer" :class="{ visible: openModalInstruction }">
+          </div>
+          <div class="focus-instruction flex flex-col gap-5" :class="{ visible: openModalInstruction }">
+          <span class="text-6xl cursor-pointer">
+            clicca qui per iniziare
+          </span>
+          <span>
+            <svg xmlns="https://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-arrow-return-left text-2xl cursor-pointer" viewBox="0 0 26 26">
+              <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5"/>
+            </svg>
+          </span>
+          </div>
         </div>
       </div>
 
@@ -224,12 +229,18 @@ export default {
     return {
       componentKey: 0,
       openModalInstruction: false,
+      isLoading: true,
     };
   },
   methods: {
     toggleModalInstruction() {
       this.openModalInstruction = !this.openModalInstruction;
     },
+  },
+  mounted() {
+      setTimeout(() => {
+        this.isLoading = false; 
+      }, 3000);
   },
 }
 
