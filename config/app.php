6f818f578ba2414ec10176c1a66240b078f7d698
Alex-Uva89 <a.uva1988@gmail.com>
2025-04-08 20:43:50 +0200

    revert to noAws

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', true),
+    'debug' => (bool) env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,8 +52,7 @@ return [
     |
     */
 
-    // 'url' => env('APP_ENV') === 'production' ? env('APP_URL_HTTPS', 'https://majestic-isle-royale-01245-f4e167ad8dbf.herokuapp.com') : env('APP_URL', 'http://localhost'),
-    'url' => env('APP_ENV') === 'production' ? env('APP_URL_HTTPS', '/') : env('APP_URL', 'http://localhost'),
+    'url' => env('APP_ENV') === 'production' ? env('APP_URL_HTTPS', 'https://majestic-isle-royale-01245-f4e167ad8dbf.herokuapp.com') : env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
