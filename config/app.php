a67eb64c59b7c407375f8aa76b5a1dcc42e4c51d
Alex-Uva89 <a.uva1988@gmail.com>
2025-04-08 17:44:07 +0200

    app.php sost 'url'

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_ENV') === 'production' ? env('APP_URL_HTTPS', 'https://majestic-isle-royale-01245-f4e167ad8dbf.herokuapp.com') : env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_ENV') === 'production' ? env('APP_URL_HTTPS', 'https://majestic-isle-royale-01245-f4e167ad8dbf.herokuapp.com') : env('APP_URL', 'http://localhost'),
+    'url' => env('APP_ENV') === 'production' ? env('APP_URL_HTTPS', '/') : env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
